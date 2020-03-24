c50154f933866f4e160c969c3c651f211386c9c8
rinatoz <57804004+rinatoz@users.noreply.github.com>
2020-03-24 16:03:04 +0200

    cpp file

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdexcept>
+#include "PhoneticFinder.hpp"
+
+using namespace std;
+
+bool phonetic::simalar(char x, char y)
+ {
+  return true;
+}
+string phonetic::is_Same_Word(string text, string word) {
+  
+    return "";
+}
+/*
+int main() {
+
+}
+*/
