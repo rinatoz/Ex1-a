71e9a4733fdd80db1250dc236b4a1abd6422b2dc
rinatoz <57804004+rinatoz@users.noreply.github.com>
2020-03-24 16:00:54 +0200

    hpp file

---
+++ PhoneticFinder.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+namespace phonetic {
+    string find(string text, string word);
+    bool simalar(char x, char y);
+    string is_Same_Word(string text, string word);
+}
